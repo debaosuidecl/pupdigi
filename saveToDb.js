f981e420177f713ddcdbe3ac3b1c73aea79eccff
clement <debaosuidecl@gmail.com>
2020-01-04 13:58:53 +0100

    stuff

--- saveToDb.js
+++ saveToDb.js
@@ -7,21 +7,24 @@ const app = express();
 const fs = require('fs');
 
 const Names = require('./model/PhoneToVerify');
-
+const saveToDbPhone = async row => {
+  try {
+    let newPhoneData = new Names({
+      phone: row['Phone']
+    });
+    const freshData = await newPhoneData.save();
+    console.log(freshData, 'data saved');
+    console.log('record saved in database');
+  } catch (error) {
+    console.log(error);
+  }
+};
 const savePhones = () => {
   fs.createReadStream('Verizon.csv')
     .pipe(csv())
     .on('data', async row => {
-      try {
-        let newPhoneData = new Names({
-          phone: row['Phone']
-        });
-        const freshData = await newPhoneData.save();
-        console.log(freshData, 'data saved');
-        console.log('record saved in database');
-      } catch (error) {
-        console.log(error);
-      }
+      // await saveToDbPhone(row)\
+      setTimeout(saveToDbPhone, 1000);
     })
     .on('end', () => {
       console.log('CSV file successfully processed');
